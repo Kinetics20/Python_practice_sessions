440ca7c38ca9e9a1d4ac6d920eb2fd37978e8c76
Kinetics20 <kinetics20@gmail.com>
2024-11-20 09:06:06 -0800

    improving_closure

--- Functional_Programming/closure_exercises.py
+++ Functional_Programming/closure_exercises.py
@@ -1,4 +1,6 @@
 # create generator numbers from 1 do infinity with step = 1
+from time import sleep
+
 
 def outer_fn():
     index = 1
@@ -71,17 +73,70 @@ def outer_fn_3(index=1):
     return inner_fn
 
 e = outer_fn_3(100)
-print(e())
-print(e(30))
-print(e(30000))
-print(e())
+# print(e())
+# print(e(30))
+# print(e(30000))
+# print(e())
 
 # create function will divide or multiply depending on the first element of collection
 
+# coll[0] >= 0 => multiply by 2
+# coll[0] <  0 => divide   by 2
 
 
-r1 = list(map(fn, [1, 2, 3, 4]))
-r2 = list(map(fn, [-1, 2, 3, 4]))
+def calculate():
 
-print(r1, r2)
+    predicate = None
+    def inner(item):
+        nonlocal predicate
 
+        if predicate is None:
+            predicate = item >= 0
+
+        if predicate:
+            return item * 2
+        return item / 2
+
+    return inner
+
+# calc = calculate()
+
+# r1 = list(map(calc, [1, 2, 3, 4]))
+r1 = list(map(calculate(), [1, 2, 3, 4]))
+# r2 = list(map(calc, [-1, 2, 3, 4]))
+r2 = list(map(calculate(), [-1, 2, 3, 4]))
+
+# print(r1, r2)
+
+# memoizing level - 1
+# memoizing - memorizing the result of a function to memory, to reuse it and avoid calculation again
+# Rules :
+# - fn has to be pure
+# - small amount of parameters combinations
+
+def memoize():
+    cache = {}
+    print('outer')
+
+    def inner(a, b):
+        print('inner')
+        key = f'{a},{b}'
+        if key not in cache:
+            # intensive CPU task
+            sleep(3)
+            cache[key] = a + b
+        return cache[key]
+
+    return inner
+
+
+# def calculate_magic(a, b):
+#     # intensive CPU task
+#     sleep(3)
+#     return a + b
+calculate_magic = memoize()
+
+print(calculate_magic(1, 2))
+print(calculate_magic(2, 2))
+print(calculate_magic(1, 2))
+print(calculate_magic(2, 2))
