7c0b75cebb3127e97889b58cee01f70b8eb7d688
Kinetics20 <kinetics20@gmail.com>
2024-11-27 00:08:42 +0100

    improvingd

--- Functional_Programming/decorators_2.py
+++ Functional_Programming/decorators_2.py
@@ -11,16 +11,40 @@ def reverse(fn):
 
     return inner
 
-
-def gen_html(fn):
+def inner_wrapper(fn, tag, tag_2):
     def inner(name):
-        result = fn(f'<b>{name}<b/>')
-        return f'<h1>{result}</h1>'
-
-
+        result = fn(f'<{tag_2}>{name}<{tag_2}/>')
+        return f'<{tag}>{result}</{tag}>'
     return inner
+
+
+
+def gen_html(cb=None, *, tag='h1',tag_2='b'):
+
+    if callable(cb):
+        return inner_wrapper(cb, tag, tag_2)
+
+
+    def wrapper(cb_):
+        return inner_wrapper(cb_, tag, tag_2)
+
+    return wrapper
+
+
+
+
+
+
+# def gen_html(fn):
+#     def inner(name):
+#         result = fn(f'<b>{name}<b/>')
+#         return f'<h1>{result}</h1>'
+#
+#     return inner
+
+
 @capitalize
-@gen_html
+@gen_html(tag_2='H')
 # @reverse
 # @capitalize
 def bye(name):
@@ -29,7 +53,7 @@ def bye(name):
 #     return f'<h1>Hello <b>{name}<b/></h1>'
 
 @capitalize
-@gen_html
+@gen_html(tag='h3',tag_2='i')
 # @capitalize
 # @reverse
 def hello(name):
