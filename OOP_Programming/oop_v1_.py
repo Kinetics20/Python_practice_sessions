ba27c1d88824b10ffd5bbff2538b6c96869befda
Kinetics20 <kinetics20@gmail.com>
2024-12-04 07:19:08 -0800

    improving oop

--- OOP_Programming/oop_v1_.py
+++ OOP_Programming/oop_v1_.py
@@ -1,5 +1,5 @@
 # class - namespace, (schema)
-from typing import Any
+from typing import Any, Sized
 
 
 # class keyword
@@ -89,8 +89,14 @@ class Auto:
     def magic():
         return 'brum!!!'
 
-    def __len__(self) -> int:
-        return 42
+    def __str__(self) -> str:
+        return self.model
+
+    # def __len__(self) -> int:
+    #     return 42
+
+    # def __repr__(self):
+    #     return f'{type(self)}'
 
     # def magic(self, new_colour):
     #     type(self).colour = new_colour
@@ -98,45 +104,53 @@ class Auto:
     # def tune(self):
     #     self.nitro = True
 
-doodge = Auto('Viper', 350, 2005)
+dodge = Auto('Viper', 350, 2005)
 porsche = Auto('911', 380, 2009)
 nissan = Auto.auto_nitro('skyline',380, 2001, True)
 
-print(nissan.nitro)
+print(dodge)
+print(Auto.__mro__)
+print(issubclass(Auto, object))
+print(issubclass(Auto, Sized))
+
+# print(nissan.nitro)
 
 # syntactic sugar
-doodge.speed_up(100)
-# type(doodge).speed_up(doodge, 60)
-# Auto.speed_up(doodge, 30)
-print(dir(Auto))
-# print(doodge.speed)
+# dodge.speed_up(100)
+# type(dodge).speed_up(dodge, 60)
+# Auto.speed_up(dodge, 30)
+# print(dir(Auto))
+# print(dodge.speed)
+
+
 
 # TODO bad practice !!! declare self out of the Class !!!!!!!
-# doodge.gearbox = 'Manual'
+# dodge.gearbox = 'Manual'
 
-# doodge.tune()
+# dodge.tune()
 
-# print(doodge.model, doodge.max_speed, doodge.year, doodge.gearbox )
-# print(doodge.model, doodge.max_speed, doodge.year, doodge.nitro)
-# print(doodge.model, doodge.max_speed, doodge.year, doodge.colour)
-# print(doodge.model, doodge.max_speed, doodge.year)
+# print(dodge.model, dodge.max_speed, dodge.year, dodge.gearbox )
+# print(dodge.model, dodge.max_speed, dodge.year, dodge.nitro)
+# print(dodge.model, dodge.max_speed, dodge.year, dodge.colour)
+# print(dodge.model, dodge.max_speed, dodge.year)
 # print(porsche.model, porsche.max_speed, porsche.year )
 
 # TODO bad practice !!! it's possible to change class property/field but we don't apply this method
-# doodge.colour = 'black'
+# dodge.colour = 'black'
 # TODO this is write way to change class field from object level by class referring
-doodge.magic = 'Gold'
+# dodge.magic = 'Gold'
 # print(Auto.colour)
-# print(doodge.colour)
+# print(dodge.colour)
 
 # TODO vars is a dict with whole properties for exact object
 
-# print(vars(Auto))
-print(vars(doodge))
-# print(vars(porsche))
-# print(dir(doodge))
-print(len(doodge))
-print(doodge.__len__())
+# # print(vars(Auto))
+# print(vars(dodge))
+# # print(vars(porsche))
+# print(dir(dodge))
+# print(len(dodge))
+# # print(dodge.__len__())
+# # print(repr(dodge))
 
 
 
