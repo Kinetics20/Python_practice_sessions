1bce3a6f29b1b22e31b37a1585562a95d3b6e9a0
Kinetics20 <kinetics20@gmail.com>
2024-12-05 02:01:54 -0800

    adding new fenctions

--- functions_500_.py
+++ functions_500_.py
@@ -5069,3 +5069,25 @@ def difference_update_2(any_set, any_set_2):
 # print(difference_update_2(set1, set2))
 assert difference_update_2(set1, set2) == set()
 
+# 522
+# Codewars function
+# Create a method all which takes two params:
+#
+#     a sequence
+#     a function (function pointer in C)
+#
+# and returns true if the function in the params returns true for every element in the sequence.
+# Otherwise, it should return false. If the sequence is empty, it should return true,
+# since technically nothing failed the test.
+#
+# Example
+#
+# all((1, 2, 3, 4, 5), greater_than_9) -> false
+# all((1, 2, 3, 4, 5), less_than_9)    -> True
+
+def _all(seq, fun):
+    return all(map(fun, seq))
+
+numbers = [2, 4, 6, 8]
+is_even = lambda x: x % 2 == 0
+assert _all(numbers, is_even) == True
