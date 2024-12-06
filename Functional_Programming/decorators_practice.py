458d022a90def2398239ab764b8d407700956687
Kinetics20 <kinetics20@gmail.com>
2024-12-06 23:17:07 +0100

    adding memoizing

--- Functional_Programming/decorators_practice.py
+++ Functional_Programming/decorators_practice.py
@@ -32,6 +32,40 @@ def say_buy(name):
     return f'Bye {name}'
 
 
-print(say_hello('jaroslaw'))
-print(say_buy('Lech'))
+# print(say_hello('jaroslaw'))
+# print(say_buy('Lech'))
 
+# memoizing
+
+def cache(fn):
+    memo = {}
+    print('cache', 20 * '-')
+    def wrapper(*args):
+        if args not in memo:
+            print('save', 20 * '#')
+            memo[args] = fn(*args)
+        return memo[args]
+
+    return wrapper
+
+@cache
+def factorial(n):
+    if n == 0:
+        return 1
+    return n * factorial(n - 1)
+
+print(factorial(10))
+print(factorial(10))
+
+
+class X:
+    def __init__(self):
+        self._value = 42
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, new_val):
+        self._value = new_val
