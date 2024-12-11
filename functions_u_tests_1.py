64ea3d9526ba7aadb43393e0c1379c3545fb2591
Kinetics20 <kinetics20@gmail.com>
2024-12-11 07:18:22 -0800

    testing pytest with functions

---
+++ functions_u_tests_1.py
@@ -0,0 +1,14 @@
+def divide(a, b):
+    return a / b
+
+
+if divide(20, 5) != 10.0:
+    print('OK')
+else:
+    raise AssertionError('Sth is wrong')
+
+try:
+    divide(20, 0)
+    raise AssertionError('Sth is wrong')
+except ZeroDivisionError:
+    print('OK')
---
+++ test_functions_u_tests_1.py
@@ -0,0 +1,5 @@
+from functions_u_tests_1 import divide
+
+
+def test_divide_positive_numbers():
+    assert divide(20, 5) == 4.0
