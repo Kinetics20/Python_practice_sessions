e8ea36d081ed7aaee9f0836e012953cb80f9ce20
Kinetics20 <kinetics20@gmail.com>
2024-12-11 07:28:11 -0800

    testing pytest with functions

--- test_functions_u_tests_1.py
+++ test_functions_u_tests_1.py
@@ -3,3 +3,15 @@ from functions_u_tests_1 import divide
 
 def test_divide_positive_numbers():
     assert divide(20, 5) == 4.0
+
+
+def test_divide_numbers_negative():
+    assert divide(20, 5) != 10.0
+
+
+
+try:
+    divide(20, 0)
+    raise AssertionError('Sth is wrong')
+except ZeroDivisionError:
+    print('OK')
