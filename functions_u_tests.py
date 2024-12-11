1ee9b330d3f7cbb8246e6ff411b93eceabd89524
Kinetics20 <kinetics20@gmail.com>
2024-12-11 06:35:12 -0800

    adding new function with assertions

---
+++ functions_u_tests.py
@@ -0,0 +1,15 @@
+def divide(a, b):
+    return a / b
+
+
+if divide(20, 5) == 4.0:
+    print('OK')
+else:
+    raise AssertionError('Sth is wrong')
+
+
+if divide(20, 5) != 10.0
+    print('OK')
+else:
+    raise AssertionError('Sth is wrong')
+
