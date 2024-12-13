cbe4057d612b1a42c1b194dfc3056d13cd72c0e9
Kinetics20 <kinetics20@gmail.com>
2024-12-13 15:56:10 +0100

    draft for common word tests

---
+++ Unit_tests/test_common_word.py
@@ -0,0 +1,22 @@
+def test_most_common_word_basic():
+    pass
+
+
+def test_most_common_word_case_insensitivity():
+    pass
+
+
+def test_most_common_word_empty_text():
+    pass
+
+
+def test_most_common_word_special_characters():
+    pass
+
+
+def test_most_common_word_tie():
+    pass
+
+
+def test_most_common_word_single_word():
+    pass
