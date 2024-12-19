54866a6765a02def2965939b9c877358d11b6fda
Kinetics20 <kinetics20@gmail.com>
2024-12-19 15:59:28 +0100

    add isinstance for True case to fn

--- Unit_tests/fn_comprehension.py
+++ Unit_tests/fn_comprehension.py
@@ -1,5 +1,5 @@
 def create_lst_(num: int) -> list[int]:
-    if not isinstance(num, int):
+    if not isinstance(num, int) or isinstance(num, bool):
         raise TypeError(f'Expected int but got {type(num)}')
     return [item for item in range(num)]
 
