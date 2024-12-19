051f66d3030faf8aee74b95b5046122e3f0a7c0d
Kinetics20 <kinetics20@gmail.com>
2024-12-19 15:10:26 +0100

    add negative test for fn_comprehension.py

--- Unit_tests/test_fn_comprehension.py
+++ Unit_tests/test_fn_comprehension.py
@@ -3,3 +3,7 @@ from Unit_tests.fn_comprehension import create_lst_
 
 def test_positive_fn_comprehension():
     assert create_lst_(5) == [0, 1, 2, 3, 4]
+
+
+def test_negative_fn_comprehension():
+    assert create_lst_(-1) == []
