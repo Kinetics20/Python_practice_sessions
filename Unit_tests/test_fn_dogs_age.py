4473e1815b6bfb77a0bfe8d24726df82650897d6
Kinetics20 <kinetics20@gmail.com>
2024-12-20 19:10:50 +0100

    add other values cases to test file

--- Unit_tests/test_fn_dogs_age.py
+++ Unit_tests/test_fn_dogs_age.py
@@ -1,3 +1,5 @@
+import pytest
+
 from Unit_tests.fn_dogs_age import dogs_age_2
 
 
@@ -7,3 +9,18 @@ def test_positive_age_case_1():
 
 def test_positive_age_case_2():
     assert dogs_age_2(3.5) != 40
+
+
+def test_negative_age_case():
+    with pytest.raises(ValueError):
+        dogs_age_2(-1)
+
+
+def test_other_value_case():
+    with pytest.raises(TypeError):
+        dogs_age_2('dog')
+
+
+def test_other_value_case_2():
+    with pytest.raises(TypeError):
+        dogs_age_2(True)
