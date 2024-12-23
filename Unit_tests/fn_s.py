b5baa38c9863ae7ecc35f4ecbf1189772b7af66e
Kinetics20 <kinetics20@gmail.com>
2024-12-23 17:06:16 +0100

    add changes in function

--- Unit_tests/fn_s.py
+++ Unit_tests/fn_s.py
@@ -1,7 +1,7 @@
 def reverse_str(s: str) -> str:
     """
     Function returns reversed string
-    :param s: The s is a parameter taken by reverse_str. Must be not empty
+    :param s: The s is a parameter taken by reverse_str function. Must be not empty.
     :type s: str
     :raises TypeError: If s is not a string
     :return: reversed string
@@ -11,5 +11,5 @@ def reverse_str(s: str) -> str:
         raise TypeError('s is not a string')
     return s[::-1]
 
-
-print(reverse_str('Where is my wallet ?'))
+if __name__ == '__main__':
+    print(reverse_str([1, 2, 3]))
