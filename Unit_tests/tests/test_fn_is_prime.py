591f56dce34d9e56d647567c82fd6406e56b61be
Kinetics20 <kinetics20@gmail.com>
2024-12-25 21:21:57 +0100

    add new cases in tests file

--- Unit_tests/tests/test_fn_is_prime.py
+++ Unit_tests/tests/test_fn_is_prime.py
@@ -7,6 +7,8 @@ def test_prime_numbers():
     assert is_prime(2) == True
     assert is_prime(5) == True
     assert is_prime(13) == True
+    assert is_prime(17) == True
+    assert is_prime(101) == True
 
 
 def test_negative_numbers():
@@ -24,9 +26,15 @@ def test_is_prime_str():
         is_prime({2: "Home", 3: 'SEa'})
     with pytest.raises(TypeError):
         is_prime(3.1525)
+    with pytest.raises(TypeError):
+        is_prime(None)
+    with pytest.raises(TypeError):
+        is_prime(True)
 
 
 def test_non_prime_numbers():
     assert is_prime(1) == False
     assert is_prime(4) == False
     assert is_prime(9) == False
+    assert is_prime(6) == False
+    assert is_prime(100) == False
