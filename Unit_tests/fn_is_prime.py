95d012d5554ed7f33a56d971c0323ff3e58e4657
Kinetics20 <kinetics20@gmail.com>
2024-12-25 21:17:28 +0100

    add changes in fn and test files

--- Unit_tests/fn_is_prime.py
+++ Unit_tests/fn_is_prime.py
@@ -9,6 +9,8 @@ def is_prime(num: int) -> bool:
     :return: bool value, True if it is a prime number, False otherwise
     :rtype: bool
     """
+    if not isinstance(num, int) or isinstance(num, bool):
+        raise TypeError(f'Expected int but got {type(num)}')
     if num <= 1:
         return False
     for i in range(2, int(math.sqrt(num)) + 1):
--- Unit_tests/tests/test_fn_is_prime.py
+++ Unit_tests/tests/test_fn_is_prime.py
@@ -1,3 +1,5 @@
+import pytest
+
 from Unit_tests.fn_is_prime import is_prime
 
 
@@ -11,3 +13,20 @@ def test_negative_numbers():
     assert is_prime(-2) == False
     assert is_prime(-1) == False
     assert is_prime(0) == False
+
+
+def test_is_prime_str():
+    with pytest.raises(TypeError):
+        is_prime("Home")
+    with pytest.raises(TypeError):
+        is_prime(["Home", 2])
+    with pytest.raises(TypeError):
+        is_prime({2: "Home", 3: 'SEa'})
+    with pytest.raises(TypeError):
+        is_prime(3.1525)
+
+
+def test_non_prime_numbers():
+    assert is_prime(1) == False
+    assert is_prime(4) == False
+    assert is_prime(9) == False
