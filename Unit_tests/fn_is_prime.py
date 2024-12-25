80bdc0a0982c768234796c2d904f8d30950b623c
Kinetics20 <kinetics20@gmail.com>
2024-12-25 20:37:26 +0100

    add docstring to fn

--- Unit_tests/fn_is_prime.py
+++ Unit_tests/fn_is_prime.py
@@ -2,6 +2,13 @@ import math
 
 
 def is_prime(num: int) -> bool:
+    """
+    Function takes number and returns true if it is a prime number
+    :param num: The num must be an integer number
+    :type num: int
+    :return: bool value, True if it is a prime number, False otherwise
+    :rtype: bool
+    """
     if num <= 1:
         return False
     for i in range(2, int(math.sqrt(num)) + 1):
