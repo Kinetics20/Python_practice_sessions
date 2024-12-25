53bdc095d502c54487700c4bed7abf1a3c291fe6
Kinetics20 <kinetics20@gmail.com>
2024-12-25 20:26:35 +0100

    add is_prime fn

---
+++ Unit_tests/fn_is_prime.py
@@ -0,0 +1,10 @@
+import math
+
+
+def is_prime(num: int) -> bool:
+    if num <= 1:
+        return False
+    for i in range(2, int(math.sqrt(num)) + 1):
+        if num % i == 0:
+            return False
+    return True
