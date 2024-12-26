4e5b70efced03a148674f279a622cae9420aba0f
Kinetics20 <kinetics20@gmail.com>
2024-12-26 19:23:16 +0100

    add negative and zero tests

--- Unit_tests/tests/test_fn_gcd.py
+++ Unit_tests/tests/test_fn_gcd.py
@@ -5,3 +5,15 @@ def test_gcd_positive_numbers():
     assert gcd(54, 24) == 6
     assert gcd(48, 18) == 6
     assert gcd(101, 103) == 1
+
+
+def test_gcd_negative_numbers():
+    assert gcd(-54, 24) == 6
+    assert gcd(54, -24) == 6
+    assert gcd(-54, -24) == 6
+
+
+def test_gcd_zero():
+    assert gcd(0, 5) == 5
+    assert gcd(5, 0) == 0
+    assert gcd(0, 0) == 0
