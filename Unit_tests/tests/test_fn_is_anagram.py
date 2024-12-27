3706955cee8daa0c2c19146a6ce7b075307f7b5a
Kinetics20 <kinetics20@gmail.com>
2024-12-27 14:37:02 +0100

    add tests for numbers and special characters

--- Unit_tests/tests/test_fn_is_anagram.py
+++ Unit_tests/tests/test_fn_is_anagram.py
@@ -11,3 +11,14 @@ def test_invalid_anagrams():
     assert is_anagram("hello", "world") == False
     assert is_anagram("python", "java") == False
     assert is_anagram("test", "best") == False
+
+
+def test_anagrams_with_spaces_and_cases():
+    assert is_anagram("Listen", "Silent") == True
+    assert is_anagram("Clint Eastwood", "Old West Action") == True
+    assert is_anagram("Astronomer", "Moon starer") == True
+
+
+def test_anagrams_with_numbers_and_special_chars():
+    assert is_anagram("123 listen!", "silent 123") == True
+    assert is_anagram("An@agr@am!", "Nag a ram!!") == True
