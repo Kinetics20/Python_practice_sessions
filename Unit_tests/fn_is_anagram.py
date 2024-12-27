a9828f597dc4a40664d46c8350933a2adaa77638
Kinetics20 <kinetics20@gmail.com>
2024-12-27 14:36:07 +0100

    add changes to fn

--- Unit_tests/fn_is_anagram.py
+++ Unit_tests/fn_is_anagram.py
@@ -1,3 +1,6 @@
+import re
+
+
 def is_anagram(str_1: str, str_2: str) -> bool:
     """
     Function checks if two strings are anagrams
@@ -12,4 +15,7 @@ def is_anagram(str_1: str, str_2: str) -> bool:
     if not isinstance(str_1, str) or not isinstance(str_2, str):
         raise TypeError("str_1 and str_2 is not of type str")
 
-    return sorted(str_1.lower()) == sorted(str_2.lower())
+    str_1_cleaned = re.sub(r"[^a-zA-Z]", "", str_1).lower()
+    str_2_cleaned = re.sub(r"[^a-zA-Z]", "", str_2).lower()
+
+    return sorted(str_1_cleaned) == sorted(str_2_cleaned)
