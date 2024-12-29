77e67503abc5ca2ce92148871d3b50399c44a678
Kinetics20 <kinetics20@gmail.com>
2024-12-29 15:14:01 +0100

    add tests for filter_odd_numbers fn

---
+++ Unit_tests/tests/test_fn_filter_e_n.py
@@ -0,0 +1,21 @@
+from Unit_tests.fn_filter_e_n import filter_odd_numbers
+
+
+def test_valid_input():
+    assert filter_odd_numbers([1, 2, 3, 4, 5]) == [1, 3, 5]
+    assert filter_odd_numbers([10, 15, 20, 25]) == [15, 25]
+    assert filter_odd_numbers([0, -1, -2, -3]) == [-1, -3]
+
+
+def test_empty_list():
+    assert filter_odd_numbers([]) == []
+
+
+def test_no_odd_numbers():
+    assert filter_odd_numbers([2, 4, 6, 8]) == []
+    assert filter_odd_numbers([0, -2, -4]) == []
+
+
+def test_all_odd_numbers():
+    assert filter_odd_numbers([1, 3, 5, 7]) == [1, 3, 5, 7]
+    assert filter_odd_numbers([-1, -3, -5]) == [-1, -3, -5]
