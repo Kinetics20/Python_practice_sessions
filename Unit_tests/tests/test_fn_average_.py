8a05b46708c4f914bb6478df1c11517482571d2b
Kinetics20 <kinetics20@gmail.com>
2024-12-30 14:53:46 +0100

    add negative test

--- Unit_tests/tests/test_fn_average_.py
+++ Unit_tests/tests/test_fn_average_.py
@@ -3,3 +3,7 @@ from Unit_tests.fn_average_ import calc_average
 
 def test_positive_numbers():
     assert calc_average([1, 2, 3, 4, 5]) == 3.0
+
+
+def test_negative_numbers():
+    assert calc_average([-1, -2, -3, -4, -5]) == -3.0
