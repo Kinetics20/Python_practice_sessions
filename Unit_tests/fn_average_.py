4d159bf17e3394b6c8e2f2fca2bad6420882d841
Kinetics20 <kinetics20@gmail.com>
2024-12-30 14:51:41 +0100

    add new fn and test files

---
+++ Unit_tests/fn_average_.py
@@ -0,0 +1,10 @@
+def calc_average(l: list[int | float]) -> float:
+    """
+    Calculates average of list
+    :param l: The list of numbers, list contains mixed types of numbers.
+    :type l: list[int | float]
+    :return: Average of list.
+    :rtype: float
+    : raises ValueError if list is empty.
+    """
+    return sum(l) / len(l)
---
+++ Unit_tests/tests/test_fn_average_.py
@@ -0,0 +1,5 @@
+from Unit_tests.fn_average_ import calc_average
+
+
+def test_positive_numbers():
+    assert calc_average([1, 2, 3, 4, 5]) == 3.0
