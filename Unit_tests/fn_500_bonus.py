b2a9f7e024518dbaa5695fe002334fe1d7032072
Kinetics20 <kinetics20@gmail.com>
2024-12-31 12:41:03 +0100

    add new fn to fn_500_bonus.py and test for it

--- Unit_tests/fn_500_bonus.py
+++ Unit_tests/fn_500_bonus.py
@@ -6,7 +6,7 @@ def factorial(n: int) -> int:
     :type n: int
     :return: int number of factorial of n.
     :rtype: int
-    :raises: ValueError if n is negative or higher than 999.
+    :raises: ValueError if n is negative.
              RecursionError if n is higher than 999.
     """
     if n < 0:
@@ -15,3 +15,25 @@ def factorial(n: int) -> int:
         raise RecursionError("n must be smaller than 999")
 
     return 1 if n == 0 else n * factorial(n - 1)
+
+
+def factorial_iterative(n: int) -> int:
+    """
+    Returns the factorial of n, using iterative method.
+    :param n: the integer.
+    :type n: int
+    :return: int number of factorial of n.
+    :rtype: int
+    :raises: ValueError if n is negative.
+    """
+    if n < 0:
+        raise ValueError("n must be positive")
+    result = 1
+    for i in range(1, n + 1):
+        result *= i
+    return result
+
+
+# def count_vowels(text: str) -> int:
+#     vowels = "aeiouAEIOU"
+#     return sum(1 for char in text if char in vowels)
