91f93cbeaa602cfb47c8efb24b16c243eb6e918c
Kinetics20 <kinetics20@gmail.com>
2025-01-02 16:09:08 +0100

    add final tests

--- Unit_tests/tests/test_matrix.py
+++ Unit_tests/tests/test_matrix.py
@@ -16,12 +16,23 @@ def test_matrix_size_3():
 
 
 def test_matrix_size_0():
-     assert create_matrix(0) == []
+    assert create_matrix(0) == []
+
 
 def test_matrix_negative_size():
-     assert create_matrix(-1) == []
+    assert create_matrix(-1) == []
 
 
 def test_matrix_invalid_type_string():
     with pytest.raises(TypeError):
         create_matrix("5")
+
+
+def test_matrix_invalid_type_float():
+    with pytest.raises(TypeError):
+        create_matrix(5.5)
+
+
+def test_matrix_invalid_type_list():
+    with pytest.raises(TypeError):
+        create_matrix([1, 2, 3])
