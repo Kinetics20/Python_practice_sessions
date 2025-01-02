9b80ae7d4041d0ab453467f0218deea0286c0fa1
Kinetics20 <kinetics20@gmail.com>
2025-01-02 15:59:24 +0100

    add tests for matrix fn

--- Unit_tests/tests/test_matrix.py
+++ Unit_tests/tests/test_matrix.py
@@ -1,3 +1,5 @@
+import pytest
+
 from Unit_tests.matrix import create_matrix
 
 
@@ -7,3 +9,19 @@ def test_matrix_size_1():
 
 def test_matrix_size_2():
     assert create_matrix(2) == [[1, 2], [3, 4]]
+
+
+def test_matrix_size_3():
+    assert create_matrix(3) == [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+
+def test_matrix_size_0():
+     assert create_matrix(0) == []
+
+def test_matrix_negative_size():
+     assert create_matrix(-1) == []
+
+
+def test_matrix_invalid_type_string():
+    with pytest.raises(TypeError):
+        create_matrix("5")
