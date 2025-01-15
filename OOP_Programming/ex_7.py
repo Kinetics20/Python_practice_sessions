394ee7c1d59c79c16e2c4cbb5043013cbe714c7c
Kinetics20 <kinetics20@gmail.com>
2025-01-15 17:51:03 +0100

    multi_inheritance, slots, examples

---
+++ OOP_Programming/ex_7.py
@@ -0,0 +1,54 @@
+class Employee:
+    __slots__ = ('name', 'age', 'position', 'salary')
+
+    def __init__(self, name, age, position, salary):
+        self.name = name
+        self.age = age
+        self.position = position
+        self.salary = salary
+
+    def increase_salary(self, percent):
+        self.salary += self.salary * (percent / 100)
+
+class Tester(Employee):
+    def run_test(self):
+        print(f'({self.name}) is testing...')
+        print('All tests passed!')
+
+class Developer(Employee):
+    def __init__(self, name, age, position, salary, tech_stack):
+        super().__init__(name, age, position, salary)
+        self.tech_stack = tech_stack
+
+    def increase_salary(self, percent, bonus=0):
+        super().increase_salary(percent)
+        self.salary += bonus
+
+e1 = Developer('John', 25,  'Software Engineer C1', 10000, 'python')
+print(e1.__slots__)
+print(e1.__dict__)
+
+# e1 = Tester('John', 25,  'Software Engineer C1', 10000)
+# e2 = Developer('Kate', 35,  'Software Engineer C1', 12000, 'java')
+#
+# e1.increase_salary(20)
+# e2.increase_salary(20, 50000)
+# print(e2.tech_stack)
+
+# print(e1.salary)
+# print(e2.salary)
+
+# print(isinstance(e1, Tester))
+# print(isinstance(e1, Employee))
+# print(isinstance(e1, object))
+
+# print(issubclass(Developer, Employee))
+# print(issubclass(Developer, object))
+# print(issubclass(Employee, object))
+# print(issubclass(Tester, Developer))
+#
+# try:
+#     # raise FloatingPointError('Floating point error')
+#     raise ZeroDivisionError('Division by zero')
+# except ArithmeticError as e:
+#     print(e)
