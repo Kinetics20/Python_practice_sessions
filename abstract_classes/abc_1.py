283a81ebad77b5c1d30287c30ae7b55cb6a6b866
Kinetics20 <kinetics20@gmail.com>
2025-01-28 17:59:37 +0100

    imporoving abstract and metaclasses continue

--- abstract_classes/abc_1.py
+++ abstract_classes/abc_1.py
@@ -1,18 +1,44 @@
+from abc import ABCMeta
+
+
 class SwordMeta(type):
 
+    def __instancecheck__(cls, instance):
+        return issubclass(type(instance), cls)
+
     def __subclasscheck__(cls, subclass):
-        return True
+        if (
+            hasattr(subclass, 'swipe') and callable(subclass.swipe)
+            and
+            hasattr(subclass, 'sharpen') and callable(subclass.sharpen)
+        ):
+            return True
+        return super().__subclasscheck__(subclass)
 
 
-class Sword(metaclass=SwordMeta):
+class Sword(metaclass=ABCMeta):
     """Abstract Base Class"""
+    @classmethod
+    def __subclasshook__(cls, subclass):
+        return (
+                (
+                        hasattr(subclass, 'swipe') and callable(subclass.swipe)
+                        and
+                        hasattr(subclass, 'sharpen') and callable(subclass.sharpen)
+                )
+                or
+                NotImplemented
+        )
+
+    def thrust(self):
+        print('Thrust!', type(self).__name__)
 
 
 class BroadSword:
     def swipe(self):
         print('Swoosh!', type(self).__name__)
 
-    def sharpe(self):
+    def sharpen(self):
         print('Shrink!', type(self).__name__)
 
 
@@ -20,5 +46,23 @@ class SamuraiSword:
     def swipe(self):
         print('Slice!', type(self).__name__)
 
-    def sharpe(self):
+    def sharpen(self):
         print('Shrink!', type(self).__name__)
+
+class Rifle:
+    def fire(self):
+        print('Bang!!', type(self).__name__)
+
+class Sabre(Sword):
+    pass
+
+@Sword.register
+class LightSabre:
+    def swipe(self):
+        print('Ffffkrrrrshhzzzwooooom..woom..woom!', type(self).__name__)
+
+samurai_sword = SamuraiSword()
+
+sabre = Sabre()
+print(issubclass(LightSabre, Sword))
+print(isinstance(sabre, Sword))
