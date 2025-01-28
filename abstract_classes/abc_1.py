17d1de72f65f9835bc3fb163e69f8174de6c7032
Kinetics20 <kinetics20@gmail.com>
2025-01-28 22:54:15 +0100

    abstarct classes improving process

--- abstract_classes/abc_1.py
+++ abstract_classes/abc_1.py
@@ -1,4 +1,4 @@
-from abc import ABCMeta
+from abc import ABCMeta, ABC, abstractmethod
 
 
 class SwordMeta(type):
@@ -16,25 +16,23 @@ class SwordMeta(type):
         return super().__subclasscheck__(subclass)
 
 
-class Sword(metaclass=ABCMeta):
+class Sword(ABC):
     """Abstract Base Class"""
-    @classmethod
-    def __subclasshook__(cls, subclass):
-        return (
-                (
-                        hasattr(subclass, 'swipe') and callable(subclass.swipe)
-                        and
-                        hasattr(subclass, 'sharpen') and callable(subclass.sharpen)
-                )
-                or
-                NotImplemented
-        )
+
+    @abstractmethod
+    def swipe(self):
+        raise NotImplementedError
+
+    @abstractmethod
+    def sharpen(self):
+        raise NotImplementedError
+
 
     def thrust(self):
         print('Thrust!', type(self).__name__)
 
 
-class BroadSword:
+class BroadSword(Sword):
     def swipe(self):
         print('Swoosh!', type(self).__name__)
 
@@ -42,7 +40,7 @@ class BroadSword:
         print('Shrink!', type(self).__name__)
 
 
-class SamuraiSword:
+class SamuraiSword(Sword):
     def swipe(self):
         print('Slice!', type(self).__name__)
 
@@ -54,15 +52,22 @@ class Rifle:
         print('Bang!!', type(self).__name__)
 
 class Sabre(Sword):
-    pass
+    def swipe(self):
+        print('Swipe!', type(self).__name__)
 
-@Sword.register
+    def sharpen(self):
+        print('Sharpen!', type(self).__name__)
+
+
+
 class LightSabre:
     def swipe(self):
-        print('Ffffkrrrrshhzzzwooooom..woom..woom!', type(self).__name__)
+        print('Ffffkrrrrshhzzzwooooom..wooom..wooom!', type(self).__name__)
 
 samurai_sword = SamuraiSword()
 
 sabre = Sabre()
 print(issubclass(LightSabre, Sword))
+print(issubclass(SamuraiSword, Sword))
 print(isinstance(sabre, Sword))
+samurai_sword.thrust()
